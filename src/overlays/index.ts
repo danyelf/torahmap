5449e64c24f708c3df4fb7921aa1a01b521e60a5
danyel fisher <danyel@gmail.com>
2026-01-04 23:18:08 -0800

    feat(overlays): add overlay index with exports
    Signed-off-by: danyel fisher <danyel@gmail.com>

---
+++ src/overlays/index.ts
@@ -0,0 +1,5 @@
+// src/overlays/index.ts
+export type { Overlay, Color } from './types.ts';
+export { registerOverlay, getOverlay, getAllOverlays } from './registry.ts';
+export { divineNamesOverlay } from './divine-names.ts';
+export { commentaryOverlay, setVerses as setCommentaryVerses } from './commentary.ts';
